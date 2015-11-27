34a240388bd24809970956d94b90f1d5664be37e
Tim Menzies <tim.menzies@gmail.com>
2015-11-26 22:42:52 -0500

    saving

---
+++ tableok.py
@@ -0,0 +1,46 @@
+from __future__ import print_function, division
+import sys
+sys.dont_write_bytecode = True
+from table import *
+
+weather="""
+outlook,
+temperature,
+humidity,?windy,play
+sunny    , 85, 85, FALSE, no  # an interesting case
+sunny    , 80, 90, TRUE , no
+overcast , 83, 86, FALSE, yes
+rainy    , 70, 96, FALSE, yes
+rainy    , 68, 80, FALSE, yes
+rainy    , 65, 70, TRUE , no
+overcast , 64, 65, TRUE , 
+yes
+sunny    , 72, 95, FALSE, no
+
+sunny    , 69, 70, FALSE, yes
+rainy    , 75, 80, FALSE, yes
+sunny    , 75, 70, TRUE , yes
+overcast , 72, 90, TRUE , yes
+overcast , 81, 75, FALSE, yes
+rainy    , 71, 91, TRUE , no
+"""
+
+@ok
+def _table1():
+  src = lambda: stringer(weather)
+  for x in tables(src):
+    print(x)
+
+@ok
+def _tables():
+  src = lambda: filer('data/weather.csv')
+  for x in tables(src):
+    print(x)
+
+@ok 
+def _table3():
+  src = lambda: zipper('data/data.zip',
+                       'weather.csv')
+  for x in tables(src):
+    print(x)
+
